e5ff5b9b672c1dfdd20d74e1f315c694714f6965
Blaine Harris <blaine91787@gmail.com>
2016-09-02 17:51:02 -0500

    updated class name

---
+++ Week01_Sep14/01_Beginner1/BlaineHarris/Beginner1.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+**
+**
+**              Name: Blaine Harris
+**              Project: Weekly-Problems-2016-2017
+**              File: 01_Beginner1.java
+**              Date: 09/02/16
+**              Language: java
+**
+**
+**
+/******************************************************************************/
+
+import java.util.Scanner;
+
+public class Beginner1 {
+
+  public static void main (String[] args) {
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("What's your favorite color?");
+    String nextString = input.nextLine();
+    System.out.println(nextString + "'s a great color! Mine's red.");
+
+  }
+
+}
