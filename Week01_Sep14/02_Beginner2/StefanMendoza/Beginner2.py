b6659ddd3e4812a59ae1ccf38aba1ac5f3a4fc0e
Stefan Mendoza <stefanmendoza.dev@gmail.com>
2016-09-02 20:43:00 -0500

    [Stefan Mendoza] Week1/Beginner2 - Python

---
+++ Week01_Sep14/02_Beginner2/StefanMendoza/Beginner2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+'''
+File: Beginner2.py
+Author: Stefan Mendoza
+Date: 2 September 2016
+Description: Week-1/Beginner-2 for KU Programming Club (2016-2017)
+'''
+
+def calculate(age):
+    return (23 - age)
+
+
+print("How old are you?")
+
+try:
+    age = int(raw_input("> "))
+
+    if age == 23:
+        print("We're the same age! Nice!")
+    elif age > 23:
+        print("I'm 23. You're {0} years older than me!".format(-1 * (calculate(age))))
+    else:
+        print("I'm 23. You're {0} years younger than me!".format(calculate(age)))
+except Exception:
+    print("Woah! Something went wrong!")
