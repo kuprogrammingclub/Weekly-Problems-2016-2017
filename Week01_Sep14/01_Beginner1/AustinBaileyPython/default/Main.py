64b9180f81e012ffc69a6dbd4950d54ff7783ae6
Austin Bailey <a604b997@ku.edu>
2016-09-06 12:49:49 -0500

    Added Python Solution to Wk1prblm1

---
+++ Week01_Sep14/01_Beginner1/AustinBaileyPython/default/Main.py
@@ -0,0 +1,9 @@
+'''
+Created on Sep 6, 2016
+
+@author: Austin Bailey
+'''
+#print('what is your favorite color?');
+x = input('What is your favorite color?');#taken from https://en.wikibooks.org/wiki/Python_Programming/Input_and_Output
+print(x);           
+print('That\'s a great color! Mine\'s red.');
