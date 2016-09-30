c1467f45d9839b1cc80db7276a121edbf2c26c14
brianquiroz216 <r229q057@ku.edu>
2016-09-30 02:06:23 -0500

    excercise 1

---
+++ Week01_Sep14/01_Beginner1/BrianQuiroz/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+  string color = "";
+  cout << "What's your favorite color?";
+  cin >> color;
+  cout << "That's a great color! My favorite color is blue.\n";
+  return (0);
+}
